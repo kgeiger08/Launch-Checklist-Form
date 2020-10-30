0c0d1767569a05761264f9039d1a389bf24d9f96
kgeiger08 <72526597+kgeiger08@users.noreply.github.com>
2020-10-29 23:24:17 -0500

    finished second part. still working on third part

--- script.js
+++ script.js
@@ -1,27 +1,79 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
+   
+   let json = [];
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+     
+      response.json().then(function(json) {
+   
+         let missionTarget = document.getElementById("missionTarget");
+         let index = 2;
+         for(let i = 0; i < json.length; i++) {
+
+         missionTarget.innerHTML = `
+                    <div>
+                    <h2>Mission Destination</h2>
+                        <ol>
+                           <li>Name: ${json[index].name}</li>
+                           <li>Diameter: ${json[index].diameter}</li>
+                           <li>Star: ${json[index].star}</li>
+                           <li>Distance from Earth: ${json[index].distance}</li>
+                           <li>Number of Moons: ${json[index].moons}</li>
+                        </ol>
+                        <img src="${json[index].image}"></img>
+                    </div>
+                        `;
+                        // index = (index + 1) % json.length;
+                     };
+                });
+            });
+   
    let form = document.getElementById("launchForm");
    form.addEventListener("submit", function(event) {
+
        let pilotInput = document.querySelector("input[name=pilotName]");
        let copilotInput = document.querySelector("input[name=copilotName");
        let fuelLevelInput = document.querySelector("input[name=fuelLevel");
        let cargoMassInput = document.querySelector("input[name=cargoMass");
+       let faultyItemsList = document.getElementById("faultyItems");
+       let pilotStatus = document.getElementById("pilotStatus");
+       let copilotStatus = document.getElementById("copilotStatus");
+       let launchStatus = document.getElementById("launchStatus");
 
        if(pilotInput.value === "" || copilotInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
            alert("All Fields Required");
            event.preventDefault();
-       };
-
-       if(!isNaN(pilotInput.value) || !isNaN(copilotInput.value)) {
+       } else if(!isNaN(pilotInput.value) || !isNaN(copilotInput.value)) {
          alert("Invalid Entry. Must be a name for Pilot Name & Copilot Name.");
          event.preventDefault();
-      }
-
-       if(isNaN(fuelLevelInput.value) || isNaN(cargoMassInput.value)) {
+      } else if(isNaN(fuelLevelInput.value) || isNaN(cargoMassInput.value)) {
           alert("Invalid Entry. Must be a number for Fuel Level & Cargo Mass.");
           event.preventDefault();
-       }
+       } else {
+            pilotStatus.innerHTML = `Pilot ${pilotInput.value} is Ready`;
+            copilotStatus.innerHTML = `Copilot ${copilotInput.value} is Ready`;
+            if(fuelLevelInput.value < 10000) {
+               let fuelStatus = document.getElementById("fuelStatus");
+               fuelStatus.innerHTML = `${fuelLevelInput.value} liters is not enough fuel for the journey.`;
+               launchStatus.innerHTML = `Shuttle not ready for launch`;
+               launchStatus.style.color = "red";
+            } else {
+               launchStatus.innerHTML = `Shuttle is ready for launch`;
+               launchStatus.style.color = "green";
+            };
+            if(cargoMassInput.value > 10000) {
+               let cargoStatus = document.getElementById("cargoStatus");
+               cargoStatus.innerHTML = `${cargoMassInput.value} kilograms is too much mass for the shuttle to take off.`;
+               launchStatus.innerHTML = `Shuttle not ready for launch`;
+               launchStatus.style.color = "red";
+            } else {
+               launchStatus.innerHTML = `Shuttle is ready for launch`;
+               launchStatus.style.color = "green";
+            };
+            faultyItemsList.style.visibility = "visible";
+            event.preventDefault();
+         }
 
    });
 });
